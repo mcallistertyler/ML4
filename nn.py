1f813a366599d94087cb0b9bafe8dbebe873ec42
Tyler McAllister <tytheguy95@gmail.com>
2019-03-26 19:02:07 +0100

    Stuff

--- nn.py
+++ nn.py
@@ -1,80 +1,88 @@
 import numpy as np
 from matplotlib import pyplot as plt
-
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+digits = load_digits()
+#print(train_test_split(digits, shuffle=False))
 # XOR
-x=np.array(([0,0],[0,1],[1,0],[1,1]))
-y=np.array(([0],[1],[1],[0]))
+x=np.array(([0,0],[0,1],[1,0],[1,1]), dtype=np.float128)
+y=np.array(([0],[1],[1],[0]), dtype=np.float128)
+
 class NN:
     def __init__(self, x, y):
-        self.input = x 
-        self.weights1 = np.random.rand(2,3)
-        self.weights2 = np.random.rand(3,4)
-        self.weights3 = np.random.rand(4,1)
-        self.y = y
-        self.output = np.zeros(y.shape)
+        self.weights1 = np.random.normal(loc=0, scale=1, size=(2,3))
+        self.weights2 = np.random.normal(loc=0, scale=1, size=(3,4))
+        self.weights3 = np.random.normal(loc=0, scale=1, size=(4,1))
     
-    def forward_prop(self,current_example):
-        self.layer1 = logistic_function(np.dot(self.input[current_example], self.weights1))
-        self.layer2 = logistic_function(np.dot(self.layer1, self.weights2))
-        self.output = logistic_function(np.dot(self.layer2, self.weights3))
+    def forward_prop(self, training_data):
+        self.layer1 = relu(np.dot(training_data, self.weights1))
+        self.layer2 = relu(np.dot(self.layer1, self.weights2))
+        self.output = relu(np.dot(self.layer2, self.weights3))
         return self.output
 
-    def back_prop(self, current_example):
-        delta = loss_function(self.y[current_example], self.output) * logistic_deriv(self.output)
-        e_weights3 = np.dot(delta, self.weights3.T)
-        d_weights3 = e_weights3 * logistic_deriv(self.layer2)
+    def back_prop(self, training_data, y):
+        self.delta = loss_function(y, self.output) * relu_derivate(self.output)
+        self.e_weights3 = self.delta.dot(self.weights3.T)
+        self.d_weights3 = self.e_weights3 * relu_derivate(self.layer2)
 
-        #delta2 = loss_function(self.y, self.output) * logistic_deriv(self.layer1)
-        e_weights2 = np.dot(d_weights3, self.weights2.T)
-        d_weights2 = e_weights2 * logistic_deriv(self.layer1)
+        self.e_weights2 = self.d_weights3.dot(self.weights2.T)
+        self.d_weights2 = self.e_weights2 * relu_derivate(self.layer1)
 
-        # delta3 = loss_function(self.y, self.output) * logistic_deriv(self.layer1)
-        e_weights1 = np.dot(d_weights2, self.weights1.T)
-        d_weights1 = e_weights1 * logistic_deriv(self.input[current_example])
+        self.e_weights1 = self.d_weights2.dot(self.weights1.T)
+        self.d_weights1 = self.e_weights1 * relu_derivate(training_data)
 
-        self.weights3 = self.weights3 - 0.1 * self.layer2.T.dot(d_weights3)
-        self.weights2 = self.weights2 - 0.1 * self.layer1.T.dot(d_weights2)
-        self.weights1 = self.weights1 - 0.1 * self.input[current_example].T.dot(d_weights1)
+        self.weights1 -= 0.09 * training_data.T.dot(self.d_weights1)
+        self.weights2 -= 0.09 * self.layer1.T.dot(self.d_weights2)
+        self.weights3 -= 0.09 * self.layer2.T.dot(self.d_weights3)
 
     def network_architecture(self):
+        print('-----------Architecture-----------')
         print("Inputs", x)
         print("Weights1", self.weights1)
         print("Activations at layer 1", self.layer1)
         print("Weights2", self.weights2)
         print("Activations at layer 2", self.layer2)
         print("Weights3", self.weights3)
-        print("Output layer", self.output)
-        print("Target output", self.y)
+        print('--------------------------------\n')
 
-def predict(network, x):
-    for i in range(0, len(x)):
-        print(x[i])
-    # network.forward_prop()
+    def predict(self, network, input_data):
+        print(input_data)
+        print('Prediction:', network.forward_prop(input_data))
 
 # Activation function
 def logistic_function(z):
-    return 1 / (1 + np.exp(-z))
+    return 1.0 / (1.0 + np.exp(-z))
 
 # Derivative function
 def logistic_deriv(z):
-    return logistic_function(z) * (1 - logistic_function(z))
+    return z * (1.0 - z)
+
+def relu(x):
+    return (1-np.exp(-2*x))/(1 + np.exp(-1*x))
+
+
+def relu_derivate(x):
+    return (1+ relu(x))*(1-relu(x))
 
 # Squared loss function
 def loss_function(target_y, output_y):
-    return 0.5 * (target_y - output_y)**2
+    loss = target_y - output_y
+    return 0.5 * np.power(loss,2)
 
 network = NN(x, y)
-training_data_counter = 0 # Hacky
-for i in range(10000):
+training_data_counter = 0
+for i in range(1000):
     #one vector of input at a time
-    #e.g pass through [0, 0] then do forward prop then back prop
+    #e.g pass through [0, 0] do forward prop then back prop
     # then switch to next training example
     # Keep doing this continually??
-    network.forward_prop(training_data_counter)
-    network.back_prop(training_data_counter)
-    training_data_counter += 1
-    if training_data_counter == 3:
-        training_data_counter = 0
+    
+    for j in range(0, len(x)):
+        network.forward_prop(x[j])
+        network.back_prop(x[j],y[j])
 
-predict(network,x)
 network.network_architecture()
+print('----------Predicition----------')
+for j in range(0, len(x)):
+    network.predict(network, x[j])
+print('--------------------------------')
