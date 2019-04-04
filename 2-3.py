8747d0a11a8f7473861a97f8622c05f9467d6c93
Tyler McAllister <tytheguy95@gmail.com>
2019-04-04 22:23:19 +0200

    Adjustments to answer some questions.

--- 2-3.py
+++ 2-3.py
@@ -5,6 +5,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 digits = load_digits()
 X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=0.25, random_state=2017)
@@ -24,37 +25,39 @@ def statistics(digits, cm):
         print('Recall:', recall(x, cm))
         print('Precision', precision(x,cm))
         print('Fmeasure', (2*recall(x,cm)*precision(x,cm)) / (recall(x,cm) + precision(x,cm)))
-        print('---------------------')
-    
+    print("\n")
 
 kVals = range(1, 30, 2)
 accuracies = []
+#Find the best k value
 for k in range(1, 30, 2):
     knn = KNeighborsClassifier(n_neighbors=k)
     knn.fit(X_train, y_train)
     score = knn.score(X_test, y_test)
     accuracies.append(score)
-## 9 is the best
+
 i = int(np.argmax(accuracies))
-print("k=%d achieved highest accuracy of %.2f%% on validation data" % (kVals[i],accuracies[i] * 100))
 
 knn = KNeighborsClassifier(n_neighbors=i)
-print('KNN score: %f' % knn.fit(X_train, y_train).score(X_test, y_test))
+knn.fit(X_train, y_train).score(X_test, y_test)
 k_pred = knn.predict(X_test)
+print('KNN Test Accuracy', accuracy_score(y_test, k_pred))
 cmat = confusion_matrix(y_test, k_pred)
 print(cmat)
 statistics(10, cmat)
 
 svm_classifier = svm.SVC(gamma=0.001)
-print('SVM score: %f' % svm_classifier.fit(X_train, y_train).score(X_test, y_test))
+svm_classifier.fit(X_train, y_train)
 s_pred = svm_classifier.predict(X_test)
+print('SVM Test Accuracy', accuracy_score(y_test, s_pred))
 svm_cmat = confusion_matrix(y_test, s_pred)
 print(svm_cmat)
 statistics(10, svm_cmat)
 
-random_forest_classifier = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
-print('Random forest score: %f' % random_forest_classifier.fit(X_train, y_train).score(X_test, y_test))
+random_forest_classifier = RandomForestClassifier(n_estimators=100, max_depth=11, random_state=0)
+random_forest_classifier.fit(X_train, y_train)
 r_pred = random_forest_classifier.predict(X_test)
+print('Random Forest Test Accuracy', accuracy_score(y_test, r_pred))
 r_cmat = confusion_matrix(y_test, r_pred)
 print(r_cmat)
 statistics(10, r_cmat)
--- 2-4.py
+++ 2-4.py
@@ -2,29 +2,33 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # XOR
-x=np.array(([1,0,0],[1,0,1],[1,1,0],[1,1,1]), dtype=float) ##prepend inputs with bias
+#x=np.array(([1,0,0],[1,0,1],[1,1,0],[1,1,1]), dtype=float) ##prepend inputs with bias
+x=np.array(([0,0],[0,1],[1,0],[1,1]), dtype=float) ##prepend inputs with bias
 y=np.array(([0],[1],[1],[0]), dtype=float)
 
 loss_values = []
 
 class NN:
     def __init__(self):
-        self.weights1 = 2*np.random.random((3,3)) - 1
-        self.weights2 = 2*np.random.random((3,1)) - 1
-        
+        # self.weights1 = 2*np.random.random((2,2)) - 1
+        # self.weights2 = 2*np.random.random((2,1)) - 1
+        self.weights1 = np.array([[ 0.80434681,-0.58827863],[ 0.25778704,0.55431251]])
+        self.weights2 = np.array([[ 0.75333271],[-0.70114993]])
     def forward_prop(self, training_data):
         self.layer1 = logistic_function(np.dot(training_data, self.weights1))
         self.output = logistic_function(np.dot(self.layer1, self.weights2))
 
     def back_prop(self, training_data, test_data):
+        error_l = squared_loss_function(test_data[0], self.output[0])
+        loss_values.append(error_l)
         error = loss_function(test_data[0], self.output[0])
         self.delta = [np.multiply(error,logistic_deriv(self.output[0]))]
         self.delta = np.asarray(self.delta)
         self.delta = self.delta[np.newaxis,:]
         self.delta2 = self.delta.dot(self.weights2.T) * logistic_deriv(self.layer1)
         # self.delta1 = self.delta2.dot(self.weights1.T) *  logistic_deriv(training_data) # left this here as a reminder to NOT do this
-        self.weights2 = self.weights2 + np.multiply(0.2, self.layer1.T[:,np.newaxis].dot(self.delta))
-        self.weights1 = self.weights1 + np.multiply(0.2, training_data.T[:,np.newaxis].dot(self.delta2))
+        self.weights2 = self.weights2 + np.multiply(0.6, self.layer1.T[:,np.newaxis].dot(self.delta))
+        self.weights1 = self.weights1 + np.multiply(0.6, training_data.T[:,np.newaxis].dot(self.delta2))
 
 # Activation function
 def logistic_function(z):
@@ -41,19 +45,23 @@ def squared_loss_function(target_y, output_y):
 
 def loss_function(target_y, output_y):
     loss = target_y - output_y
-    loss_values.append(loss)
     return loss
 
 network = NN()
-for i in range(10000):
+print('Printing initial weights')
+print(network.weights1)
+print(network.weights2)
+print('-----------------------')
+for i in range(100000):
     for j in range(0,len(x)):
         network.forward_prop(x[j])
         network.back_prop(x[j],y[j])
-
-abs_loss = [ abs(x) for x in loss_values ]
-# print(abs_loss)
-epochs_trained = range(1, len(loss_values) + 1)
-plt.plot(abs_loss)
+print('Printing ending weights')
+print(network.weights1)
+print(network.weights2)
+print('-----------------------')
+epoch_count = range(1, len(loss_values) + 1)
+plt.plot(epoch_count, loss_values)
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
 network.forward_prop(x[0])
